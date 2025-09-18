391551a402e44e5a0d4941c7c5cc3a22c3b7a778
Kevenoliveiramartins <208955932+kevenoliveiramartins@users.noreply.github.com>
2025-09-18 11:18:52 -0300

    atividade

---
+++ sparta/js/app.js
@@ -0,0 +1,13 @@
+let aulas = [];
+let professores = [];
+let alunos = [];
+
+// login Admin
+
+let admin = {
+  email: "admin@gmail.com",
+    senha: "admin123",
+    nome: "Administrador"
+};
+
+// pagina do admin
